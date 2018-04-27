f6dafc0df6a4ca2596bdeb35d90f46d9aa7695ba
Adrien Mariez <adrien.mariez@gmail.com>
2018-04-27 15:59:50 +0200

    removed broken links from views

--- web/themes/bootstrap_albagnac/js/dso.js
+++ web/themes/bootstrap_albagnac/js/dso.js
@@ -1,9 +1,13 @@
-console.log("Le fichier DSO est fonctionnel");
 var button_up = document.createElement("DIV");
 button_up.id = "ok"
 document.body.appendChild(button_up);
 
 // creation svg //
+var svg = "<svg width=\"50\" height=\"50\">";
+svg+="<line x1=\"10\" y1=\"40\" x2=\"25\" y2=\"10\" style=\"stroke:rgb(255,255,255); stroke-width:2\"/>"
+svg+="<line x1=\"25\" y1=\"10\" x2=\"40\" y2=\"40\" style=\"stroke:rgb(255,255,255); stroke-width:2\"/>"
+svg+="</svg>"
+button_up.innerHTML=(svg);
 
 
 document.addEventListener("scroll",hideButton);
